0c6d547be908697616580151cc1eac527277a5f7
Diego Fleury <dfleury@gmail.com>
2011-05-27 14:35:42 -0300

    Testing if class Fairu is avaliable in fairu module

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,12 +1,17 @@
 import unittest
 
+from inspect import isclass
+
 import fairu
 
 class  CoreTestCase(unittest.TestCase):
 
-    def test_if_the_meta_informations_are_ok(self):
-        assert fairu.__author__ == "Diego Fleury"
+    def test_if_have_a_version(self):
         assert fairu.__version__ == "0.1"
+
+    def test_if_Fairu_class_is_avaliable_importing_fairu_module(self):
+        assert getattr(fairu, 'Fairu', False) != False
+        assert isclass(fairu.Fairu)
 
 if __name__ == '__main__':
     unittest.main()
